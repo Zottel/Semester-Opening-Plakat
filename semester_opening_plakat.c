a9c1db5534de2a049deed258b893ffd5dfa89d6d
Julius Roob <julius@juliusroob.de>
2013-03-30 15:12:00 +0100

    moar comments

--- semester_opening_plakat.c
+++ semester_opening_plakat.c
@@ -2,6 +2,10 @@
 #include <avr/interrupt.h>
 #include <stdlib.h>
 
+// Initial values of channels.
+// * will be incremented by 1 each step
+// * modulo 1024
+// * brightest at 256, off from 512 to 1024
 int channels[] = {0x000,
                   0x060,
                   0x0c0,
@@ -15,13 +19,14 @@ int channels[] = {0x000,
                   0x360,
                   0x3c0};
 
+// set output direction of led pins
 void setupChannels() {
 	DDRB |= (1 << PB0) | (1 << PB1) | (1 << PB2);
 	DDRA |= (1 << PA0) | (1 << PA1);
 	DDRD |= (1 << PD0) | (1 << PD1) | (1 << PD2) | (1 << PD3) | (1 << PD4) | (1 << PD5) | (1 << PD6);
 }
 
-//__attribute__((always_inline)) static void setChannel(int channel, int value) {
+// set specific channel on or off (value = 0 | 1)
 static void setChannel(int channel, int value) {
 	switch(channel) {
 		case 0:
@@ -82,12 +87,16 @@ int main() {
 	
 	while(1) {
 		asm volatile ("nop");
+		// increase brightness for each channel
 		for(int i = 0; i < 12; i++) {
 			channels[i] = (channels[i] + 1) % 1024;
 		}
+		
+		// Initialize led pins for pwm cycles
 		for(int n = 0; n < 12; n++) {
 			setChannel(n, 0);
 		}
+		// 256 pwm steps - have each led be on/off according to specific brightness
 		for(int i = 0; i < 256; i++)
 			for(int n = 0; n < 12; n++) {
 				if(i < abs(channels[n] - 256)) {
